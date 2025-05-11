93f617b129030712260d22a434e09a94ede96a0b
Liban Yonis <assadyonis@gmail.com>
2025-05-12 00:26:55 +0300

    lint error rule disabled

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,7 @@ const eslintConfig = [
     extends: ["next/core-web-vitals", "next/typescript"],
     rules: {
       "@typescript-eslint/no-non-null-asserted-optional-chain": "off",
+      "@typescript-eslint/no-unused-vars": "off",
     },
   }),
   // ...compat.extends("next/core-web-vitals", "next/typescript"),

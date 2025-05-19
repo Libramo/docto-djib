121439748dad18ea6e6d9782e855cb2d55fe062e
Liban Yonis <assadyonis@gmail.com>
2025-05-19 22:36:31 +0300

    added several features

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,4 +1,3 @@
-import DoctorList from "@/components/DoctorList";
 import Brands from "@/components/Frontend/Brands";
 import Hero from "@/components/Frontend/Hero";
 import TabbedSection from "@/components/Frontend/TabbedSection";
